00ead4809c9fab9ad6027febc3df58fdd464609d
Mahdi Raad <mehdiraad1382@gmail.com>
2025-07-01 17:32:05 +0330

    Fix formatting

--- CommentSystem.Application/Interfaces/ICommentRepository.cs
+++ CommentSystem.Application/Interfaces/ICommentRepository.cs
@@ -7,7 +7,10 @@ public interface ICommentRepository
 {
     Task AddAsync(Comment comment);
     Task<Comment?> GetByIdAsync(int id);
-    Task<IEnumerable<Comment>> GetByHotelIdAndStatusAsync(int hotelId, CommentStatus status = CommentStatus.Approved);
+
+    Task<IEnumerable<Comment>> GetByHotelIdAndStatusAsync(int hotelId,
+        CommentStatus status = CommentStatus.Approved);
+
     Task<IEnumerable<Comment>> GetByUserIdAsync(int userId);
     Task<IEnumerable<Comment>> GetAllAsync(CommentStatus? status = null);
     Task<bool> IsBookingAvailableForCommentAsync(int bookingId, int userId);
